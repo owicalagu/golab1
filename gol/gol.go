534752f70de80d5985eb8ef97728d2d4173c350e
hh20889 <hh20889@bris.ac.uk>
2021-09-27 18:00:43 +0100

    gol

--- gol/gol.go
+++ gol/gol.go
@@ -1,6 +1,55 @@
 package main
 
+func wminus(a, b, max int) int { // minus with wrapping
+	res := a - b
+	if res < 0 {
+		res = max + res + 1
+	}
+	return res
+}
+
+func wplus(a int, b, max int) int { // plus with wrapping
+	res := a + b
+	if res > max {
+		res = res - max - 1
+	}
+	return res
+}
+
+func sumCellNeighbours(col, pos, width, height int, world [][]byte) int {
+	ret := byte(0)
+
+	ret += world[col][wplus(pos, 1, height)]  // u
+	ret += world[col][wminus(pos, 1, height)] // d
+	ret += world[wminus(col, 1, width)][pos]  // l
+	ret += world[wplus(col, 1, width)][pos]   // r
+
+	ret += world[wminus(col, 1, width)][wplus(pos, 1, height)]  // ul
+	ret += world[wplus(col, 1, width)][wplus(pos, 1, height)]   // ur
+	ret += world[wminus(col, 1, width)][wminus(pos, 1, height)] // dl
+	ret += world[wplus(col, 1, width)][wminus(pos, 1, height)]  // dr
+
+	return int(ret) / 255
+}
+
 func calculateNextState(p golParams, world [][]byte) [][]byte {
+	width := p.imageWidth
+	height := p.imageHeight
+	turns := p.turns
+
+	newworld := world
+
+	for i, col := range world {
+		for j, val := range col {
+			sum := sumCellNeighbours(i, j, width, height, world)
+			if val == 255 { // live
+				if sum < 2 {
+					newworld[i][j] = 0
+				}
+			}
+		}
+	}
+
 	return world
 }
 
--- sequences/sequences.go
+++ sequences/sequences.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func addOne(a int) int {
 	return a + 1
 }
@@ -12,14 +14,36 @@ func double(slice []int) {
 	slice = append(slice, slice...)
 }
 
-func mapSlice(f func(a int) int, slice []int) {
-
+func mapSlice(f func(a int) int, slice []int) []int {
+	var newslice []int
+	for _, val := range slice {
+		newslice = append(newslice, f(val))
+	}
+	return newslice
 }
 
-func mapArray(f func(a int) int, array [3]int) {
-
+func mapArray(f func(a int) int, array [3]int) [3]int {
+	var newarr [3]int
+	for i, val := range array {
+		newarr[i] = f(val)
+	}
+	return newarr
 }
 
 func main() {
+	intsSlice := []int{1, 2, 3}
+	intsSlice = mapSlice(addOne, intsSlice)
+	fmt.Println(intsSlice)
+
+	intsArray := [3]int{1, 2, 3}
+	intsArray = mapArray(addOne, intsArray)
+	fmt.Println(intsArray)
+
+	newSlice := intsSlice[1:3]
+	newSlice = mapSlice(square, newSlice)
+	fmt.Println(newSlice)
+
+	// arrays are passed by value; slices are passed by reference
+	// (well, by value of a struct with length and a pointer to the start)
 
 }
